725891791689c47b010cd0333c3a38101b1bac32
redaAssemghor <assemghor.reda@gmail.com>
2024-06-11 20:31:33 +0100

    saas finder project

--- projectsData.js
+++ projectsData.js
@@ -1,10 +1,18 @@
 export const data = [
   {
+    name: "Micro Saas Finder",
+    description:
+      "Micro SaaS Finder is a web app that generates Micro SaaS ideas using the OpenAI API, helping entrepreneurs find marketable concepts quickly. Built with Next.js, React, and Tailwind CSS, it features robust authentication with Clerk and real-time backend services via Convex.",
+    imgurl: "/saas-finder.png",
+    repo: "https://github.com/redaAssemghor/micro-saas-finder",
+    deployUrl: "https://micro-saas-finder.vercel.app",
+  },
+  {
     name: "payloadcms Ecommerce template",
     description:
       "E-commerce template for Payload CMS, utilizing Next.js, TypeScript, MongoDB, and Stripe to ensure a robust platform with secure user authentication and an efficient admin panel for site management. This project demonstrates a deep understanding of web application development and database optimization.",
     imgurl: "/payloadcms.png",
-    repo: "",
+    repo: "https://github.com/redaAssemghor/payload-commerce",
     deployUrl: "",
   },
   {
